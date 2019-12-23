2052c6d7d62f1cf1579b113a9127cd9da21fbd63
Douglas Sousa <douglascamilo@gmail.com>
2019-12-23 13:15:35 -0300

    Criação do novo módulo usuario

--- src/app/usuarios/usuarios.module.ts
+++ src/app/usuarios/usuarios.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UsuariosComponent } from './usuarios.component';
+import { MainDivModule } from '../shared/main-div/main-div.module';
 
 @NgModule({
   declarations: [UsuariosComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MainDivModule
   ],
   exports: [
     UsuariosComponent
