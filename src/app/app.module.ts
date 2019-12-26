1fb80ee3ab1f161e970790496f8e8bc959887716
Douglas Sousa <douglascamilo@gmail.com>
2019-12-26 03:28:58 -0300

    Adicionado ng-event-bus ao projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutModule } from './layout/layout.module';
 import { CadastrosModule } from './cadastros/cadastros.module';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgEventBus } from 'ng-event-bus';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    NgbModule,
     BrowserModule,
     AppRoutingModule,
     LayoutModule,
     CadastrosModule
   ],
-  providers: [],
+  providers: [NgEventBus],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
